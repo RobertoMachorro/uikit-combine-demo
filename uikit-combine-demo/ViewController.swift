6841993a13107d4522131680c5193699ebdab335
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-02-13 16:58:54 -0500

    Updating label from Combine.

--- uikit-combine-demo/ViewController.swift
+++ uikit-combine-demo/ViewController.swift
@@ -9,27 +9,28 @@ import UIKit
 import Combine
 
 class ViewController: UIViewController {
-
+	
 	@IBOutlet weak var outputLabel: UILabel!
 	@IBOutlet weak var textField: UITextField!
-
+	
 	@Published var currentText = ""
 	var updateSubscription: AnyCancellable?
-
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
-		self.updateSubscription = $currentText.sink {
-			print("Updated text: \($0)")
-		}
+		
+		self.updateSubscription = $currentText
+			.map { "Entered: \($0)" }
+			.receive(on: RunLoop.main)
+			.assign(to: \.text, on: outputLabel)
 	}
-
+	
 	@IBAction func textEditingChanged(_ sender: Any) {
 		self.currentText = textField.text ?? "-Empty-"
 	}
-
+	
 	@IBAction func buttonTap(_ sender: Any) {
 		self.currentText = "Starting over."
 	}
-
+	
 }
